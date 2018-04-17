9cc7ea58c0c2c365557957187c1730265937e57d
Keith Myers-Crum <kcrum@civisanalytics.com>
2018-04-17 17:12:47 -0500

    BUG: added __version__ to __init__.py so folks can get the version number.

--- civismlext/__init__.py
+++ civismlext/__init__.py
@@ -3,3 +3,4 @@ from civismlext.stacking import StackedClassifier  # NOQA
 from civismlext.nonnegative import NonNegativeLinearRegression  # NOQA
 from civismlext.hyperband import HyperbandSearchCV  # NOQA
 from civismlext.preprocessing import DataFrameETL  # NOQA
+from civismlext._version import __version__  # NOQA
